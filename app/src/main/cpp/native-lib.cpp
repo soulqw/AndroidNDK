f38dd1592c7d69642f79e8f9a4d47623d4044bb6
George <cd5160866@126.com>
2020-05-10 14:46:11 +0800

    firsr commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,25 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+#include <iostream>
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_qw_androidndksample_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_qw_androidndksample_MainActivity_test1(JNIEnv *env,
+                                                jobject /* this */,
+                                                jint intValue) {
+    // accept boolean from java
+    int age = intValue;
+    __android_log_print(1, "TEST", "int:%d", age);
+
+}
